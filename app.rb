6016e0e9442e0d9a283392454131f25335c12702
Yevgeniy <anigeka@gmail.com>
2020-12-18 13:31:30 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,24 @@
+require 'date'
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
+	end
+
+	get '/hello' do
+		status 200
+		erb :hello
+	end
+	
+	get '/goodbye' do
+		status 200
+		erb :goodbye
+	end
+
+	get '/date' do
+		@date = Date.today.strftime("%A, %B %d, %Y")
+		status 200
+		erb :date
 	end
 
 
